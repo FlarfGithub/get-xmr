ed2d9a7c4f209446d414c1078824b25baba212af
FlarfGithub <84773791+FlarfGithub@users.noreply.github.com>
2022-09-02 17:01:34 +0300

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar xvzf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0
-./xmrig --coin=XMR -o xmr.2miners.com:2222 -u 445zXmdPiWgCrqiobeVRzJgQk16xz1r6B3bDvGDaJAh5XetU1kseAPwg8qUG774ysQ6Z3jZ5JaDp98jhVfZap3N4B3FGun8 -k --tls --rig-id GC-US
+./xmrig --coin=XMR -o xmr.2miners.com:2222 -u 445zXmdPiWgCrqiobeVRzJgQk16xz1r6B3bDvGDaJAh5XetU1kseAPwg8qUG774ysQ6Z3jZ5JaDp98jhVfZap3N4B3FGun8.GC-US -p x
